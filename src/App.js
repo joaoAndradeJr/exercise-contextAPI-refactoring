78bbe1fb24bfbcea8eeecd1267aa885a4613fc0c
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-21 21:28:34 -0400

    exercicio 1 resolvido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import CarsContext from './CarsContext/carsContext';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        redCar: false,
+        blueCar: false,
+        yellowCar: false,
+      },
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(car, moveCar) {
+    this.setState({ cars: { ...this.state.cars, [car]: moveCar } });
+  }
+
+  render() {
+    return (
+      <CarsContext.Provider value={ { ...this.state, handleClick: this.handleClick } }>
+        <Cars />
+      </CarsContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,71 +1,82 @@
 // src/Cars.jsx
-
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+// import { moveCar } from './redux/actionCreators';
+import CarsContext from './CarsContext/carsContext';
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
-  return (
-    <div>
-      <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
-  );
+// function Cars({ redCar, blueCar, yellowCar, moveCar }) {
+class Cars extends React.Component {
+  render() {
+    return (
+      <CarsContext.Consumer>
+        { 
+          ({ cars, handleClick }) => (
+            <div>
+              <div>
+                <img
+                  className={cars.redCar ? 'car-right' : 'car-left'}
+                  src={carRed}
+                  alt="red car"
+                />
+                <button
+                  onClick={() => handleClick('redCar', !cars.redCar)}
+                  type="button"
+                >
+                  Move
+                </button>
+              </div>
+              <div>
+                <img
+                  className={cars.blueCar ? 'car-right' : 'car-left'}
+                  src={carBlue}
+                  alt="blue car"
+                />
+                <button
+                  onClick={() => handleClick('blueCar', !cars.blueCar)}
+                  type="button"
+                >
+                  Move
+                </button>
+              </div>
+              <div>
+                <img
+                  className={cars.yellowCar ? 'car-right' : 'car-left'}
+                  src={carYellow}
+                  alt="yellow car"
+                />
+                <button
+                  onClick={() => handleClick('yellowCar', !cars.yellowCar)}
+                  type="button"
+                >
+                  Move
+                </button>
+              </div>
+            </div>
+          )
+        }
+      </CarsContext.Consumer>
+    );
+  }
 }
 
-Cars.propTypes = {
-  moveCar: PropTypes.func.isRequired,
-  blueCar: PropTypes.bool.isRequired,
-  redCar: PropTypes.bool.isRequired,
-  yellowCar: PropTypes.bool.isRequired,
-};
+// Cars.propTypes = {
+//   moveCar: PropTypes.func.isRequired,
+//   blueCar: PropTypes.bool.isRequired,
+//   redCar: PropTypes.bool.isRequired,
+//   yellowCar: PropTypes.bool.isRequired,
+// };
 
-const mapStateToProps = (state) => ({
-  redCar: state.cars.red,
-  blueCar: state.cars.blue,
-  yellowCar: state.cars.yellow});
+// const mapStateToProps = (state) => ({
+//   redCar: state.cars.red,
+//   blueCar: state.cars.blue,
+//   yellowCar: state.cars.yellow});
 
-const mapDispatchToProps = { moveCar };
+// const mapDispatchToProps = { moveCar };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+// export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+
+export default Cars;
